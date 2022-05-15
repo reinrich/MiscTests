09662c6f2a00638ea16e99bee0cb5a7b18ffde43
Richard Reingruber <richard.reingruber@sap.com>
2022-05-15 08:47:25 +0200

    modified:   Tests/src/test/loom/TestStartVirtual.java

--- Tests/src/test/loom/TestStartVirtual.java
+++ Tests/src/test/loom/TestStartVirtual.java
@@ -3,20 +3,23 @@ package test.loom;
 
 // javac -d classes --enable-preview --release 18 ~/priv/git/reinrich/MiscTests/Tests/src/test/loom/TestStartVirtual.java
 // java --enable-preview -cp classes test.loom.TestStartVirtual
-public class TestStartVirtual {
+public class TestStartVirtual implements Runnable {
 
     public static void main(String[] args) {
-        Thread t = Thread.startVirtualThread(() -> {
-            System.out.println("Hello World");
-            System.out.println("Hello World");
-            System.out.println("Hello World");
-        });
+        Thread t = Thread.startVirtualThread(new TestStartVirtual());
         try {
             t.join();
         } catch (InterruptedException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Hello Virtual World!");
+        System.out.println("Hello Virtual World!");
+        System.out.println("Hello Virtual World!");
     }
 
 }
