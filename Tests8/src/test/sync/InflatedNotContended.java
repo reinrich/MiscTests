4592a9dcfffb2690f1c9c5868f8d0c20ecd356f7
Richard Reingruber <richard.reingruber@sap.com>
2024-02-05 17:09:26 +0100

    Cleanup Tests8/src/test/sync/InflatedNotContended.java

--- Tests8/src/test/sync/InflatedNotContended.java
+++ Tests8/src/test/sync/InflatedNotContended.java
@@ -1,53 +1,34 @@
 package test.sync;
-/*
-
-run with:
-
-~/skripte/run_quick_test.sh
-
-or
-
-./sapjvm_8/bin/java
--XX:+ClientCompiler
--XX:+PrintLIR
--XX:-PrintIR
--XX:+TraceCompilerOracle
--XX:+PrintCompilation
--XX:CICompilerCount=1
--Xbatch
-'-XX:CompileCommand=compileonlywithinlining,*::testMethod*'
-'-XX:CompileCommand=dontinline,*::dontinline*'
--cp
-/u/w/d/jtests/QuickJavaTests/bin
-test.sync.InflatedNotContended
-
- */
 
 public class InflatedNotContended {
 
+    public static class TestLock {}
+
+    public static TestLock testLockInstance = new TestLock();
+    public static boolean warmupDone;
+
     public static void main(String[] args) {
-        InflatedNotContended lock = new InflatedNotContended();
-        for(int i = 0; i < 20000; i++) {
-            if ((i%100) == 0) {
-                lock.dontinline_inflate();
-            }
-            lock.testMethod();
+        int iterations = 100*1000;
+        for (int i = 0; i < iterations; i++) {
+            dontinline_dojit_testMethod();
+        }
+        warmupDone = true;
+        dontinline_dojit_testMethod();
+    }
+
+    public static void dontinline_dojit_testMethod() {
+        synchronized (testLockInstance) {
+            dontinline_inflate();
         }
     }
 
-    public synchronized void testMethod() {
-//        dontinline_inflate();
-    }
-
-    private synchronized void dontinline_inflate() {
-        // get lock inflated by calling wait()
-        try {
-            wait(1);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            System.exit(1);
-        }            
+    private static void dontinline_inflate() {
+        if (warmupDone) {
+            // get lock inflated by calling wait()
+            try {
+                testLockInstance.wait(1);
+            } catch (InterruptedException e) { /* ignored */ }
+        }
     }
 
 }
