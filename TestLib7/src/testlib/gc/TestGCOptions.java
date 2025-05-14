f97accdd626488088845775227ff90387bfca72e
Richard Reingruber <richard.reingruber@sap.com>
2025-05-14 12:18:26 +0200

    TestGCOptions: allow passing option values as property

--- TestLib7/src/testlib/gc/TestGCOptions.java
+++ TestLib7/src/testlib/gc/TestGCOptions.java
@@ -198,7 +198,7 @@ public class TestGCOptions {
     public static void initForG1OnBdw214(TestGCOptions gcOpts) {
         gcOpts.trc_level = 3;
         gcOpts.obj_header_size_bytes = 16; // roughly true for 64 bit
-        gcOpts.immortal_obj_heap_occupancy = 0.15f;
+        gcOpts.immortal_obj_heap_occupancy = getProperty("gcOpts.immortal_obj_heap_occupancy", 0.15f);
         gcOpts.immortal_obj_size_bytes = 1*K;
         gcOpts.immortal_obj_heap_occupancy_bytes = (long) (gcOpts.immortal_obj_heap_occupancy * JavaHeap.MAX_JAVA_HEAP_BYTES);
         gcOpts.immortal_obj_count = gcOpts.immortal_obj_heap_occupancy_bytes / gcOpts.immortal_obj_size_bytes;
@@ -206,10 +206,10 @@ public class TestGCOptions {
         gcOpts.mortal_obj_size_bytes = 256;
         gcOpts.mortal_obj_heap_occupancy_bytes = (long) (gcOpts.mortal_obj_heap_occupancy * JavaHeap.MAX_JAVA_HEAP_BYTES);
         gcOpts.mortal_obj_count = gcOpts.mortal_obj_heap_occupancy_bytes / gcOpts.mortal_obj_size_bytes;
-        gcOpts.alloc_percentage_immortal = 20;
-        gcOpts.alloc_percentage_mortal   = 10;
+        gcOpts.alloc_percentage_immortal = (int)getProperty("gcOpts.alloc_percentage_immortal", 10);
+        gcOpts.alloc_percentage_mortal   = (int)getProperty("gcOpts.alloc_percentage_mortal", 2);;
         gcOpts.alloc_percentage_short_lived = 100-(gcOpts.alloc_percentage_immortal+gcOpts.alloc_percentage_mortal);
-        gcOpts.busy_wait_iterations = 1000;
+        gcOpts.busy_wait_iterations = getProperty("gcOpts.busy_wait_iterations", 1000);
     }
 
     public static void initForG1OnLu0486(TestGCOptions gcOpts) {
@@ -227,5 +227,23 @@ public class TestGCOptions {
         gcOpts.alloc_percentage_mortal   = 10;
         gcOpts.alloc_percentage_short_lived = 100-(gcOpts.alloc_percentage_immortal+gcOpts.alloc_percentage_mortal);
         gcOpts.busy_wait_iterations = 1000;
+    }
+
+    static long getProperty(String testProp, long dflt) {
+        long res = dflt;
+        try {
+            res = Long.valueOf(System.getProperty(testProp));
+        } catch(Throwable e) { // ignore and return dflt
+        }
+        return res;
+    }
+
+    static float getProperty(String testProp, float dflt) {
+        float res = dflt;
+        try {
+            res = Float.valueOf(System.getProperty(testProp));
+        } catch(Throwable e) { // ignore and return dflt
+        }
+        return res;
     }
 }
