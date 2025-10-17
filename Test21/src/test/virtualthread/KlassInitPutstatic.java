71cd070b658dc89bb1d39f857f2aaa646aaf7d09
Richard Reingruber <richard.reingruber@sap.com>
2025-10-17 09:17:23 +0200

    Adding Test21/src/test/virtualthread/KlassInitPutstatic.java

---
+++ Test21/src/test/virtualthread/KlassInitPutstatic.java
@@ -0,0 +1,65 @@
+package test.virtualthread;
+
+import java.util.concurrent.CountDownLatch;
+
+import testlib.TestBase;
+
+public class KlassInitPutstatic  extends TestBase {
+//    private static final int MAX_VTHREAD_COUNT = 8 * Runtime.getRuntime().availableProcessors();
+    private static int MAX_VTHREAD_COUNT;
+
+    private static final CountDownLatch finishPutStatic = new CountDownLatch(1);
+
+    public static void main(String[] args) {
+        try {
+            MAX_VTHREAD_COUNT = Integer.parseInt(args[0]);
+            System.out.println("MAX_VTHREAD_COUNT:" + MAX_VTHREAD_COUNT);
+            testReleaseAtKlassInitPutStatic();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    static void testReleaseAtKlassInitPutStatic() throws Exception {
+        class TestClass {
+            static {
+                try {
+                    finishPutStatic.await();
+                } catch (InterruptedException e) {}
+            }
+            public static int NUMBER;
+        }
+
+        Thread[] vthreads = new Thread[MAX_VTHREAD_COUNT];
+        CountDownLatch[] started = new CountDownLatch[MAX_VTHREAD_COUNT];
+        for (int i = 0; i < MAX_VTHREAD_COUNT; i++) {
+            final int id = i;
+            started[i] = new CountDownLatch(1);
+            vthreads[i] = Thread.ofVirtual().start(() -> {
+                started[id].countDown();
+                TestClass.NUMBER = id;
+            });
+        }
+        for (int i = 0; i < MAX_VTHREAD_COUNT; i++) {
+            started[i].await();
+            await(vthreads[i], Thread.State.WAITING);
+        }
+
+        finishPutStatic.countDown();
+        for (int i = 0; i < MAX_VTHREAD_COUNT; i++) {
+            vthreads[i].join();
+        }
+    }
+
+    /**
+     * Waits for the given thread to reach a given state.
+     */
+    private static void await(Thread thread, Thread.State expectedState) throws InterruptedException {
+        Thread.State state = thread.getState();
+        while (state != expectedState) {
+            Thread.sleep(10);
+            state = thread.getState();
+        }
+    }
+}
